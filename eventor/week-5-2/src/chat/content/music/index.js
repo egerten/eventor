93ff6c951257401491e53a633b3fa76d7e637826
Edagul Erten <edagul@sabanciuniv.edu>
2017-03-11 22:14:13 +0300

    Solve link problem

--- eventor/week-5-2/src/chat/content/music/index.js
+++ eventor/week-5-2/src/chat/content/music/index.js
@@ -1,6 +1,7 @@
 var controller = require('./controller');
 
 angular
+    .module('chat-app')
     .component('music', {
         templateUrl: 'chat/content/music/template.html',
         controller
