044551a0510170fd09b9d0257174dffc92d00506
2005508097 <gokhansaman@gmail.com>
2017-04-02 01:26:17 +0300

    Fix indentation

--- eventor/week-5-2/src/chat/content/eventsMap/controller.js
+++ eventor/week-5-2/src/chat/content/eventsMap/controller.js
@@ -1,16 +1,7 @@
 module.exports = class EventsMapController {
     constructor(MapService) {
         this.map = { center: { latitude: 40.892526, longitude: 29.374910 }, zoom: 8 };
-        /*this.marker = [
-            {
-                id: 583187,
-                icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                latitude: 45,
-                longitude: -73,
-                title: "title"
-            }
-        ];*/
-        this.mapService=MapService;
+        this.mapService = MapService;
         this.maps = MapService.maps;
         this.mapService.getMaps();
         this.mapService.bind('new maps', () => {
@@ -18,13 +9,6 @@ module.exports = class EventsMapController {
             console.log(this.maps);
             console.log(this.maps[1][2].center.latitude);
         });
-      /* this.windowOpt = {
-            position: {
-                lat: 45.05,
-                lng: -73.05
-            },
-            show: true
-        };*/
 
     }
     showWindow() {
